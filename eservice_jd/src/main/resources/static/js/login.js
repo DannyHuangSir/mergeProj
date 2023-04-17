3f9d5df095a1c86320004620a3fb87a8a6c44675
mrtang <re110119120@163.com>
2023-04-17 13:16:51 +0800

    bugfix

--- eservice_jd/src/main/resources/static/js/login.js
+++ eservice_jd/src/main/resources/static/js/login.js
@@ -12,18 +12,6 @@ window.fbAsyncInit = function() {
 
 };
 
-(function(d, s, id) {
-	var js, fjs = d.getElementsByTagName(s)[0];
-	if (d.getElementById(id)) {
-		return;
-	}
-	js = d.createElement(s);
-	js.id = id;
-	js.src = "https://connect.facebook.net/en_US/sdk.js";
-	fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-
 function checkLoginState() {
 	//非使用帳號密碼登入，清空用戶名稱及密碼欄
 	$('#userId').val("");
