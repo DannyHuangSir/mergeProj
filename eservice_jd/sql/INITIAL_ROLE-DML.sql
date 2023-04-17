ce6bf941dfb5458598ac601d6dfee771b75c57d7
mrtang <re110119120@163.com>
2023-04-17 16:09:15 +0800

    通路名称bugfix

--- eservice_jd/sql/INITIAL_ROLE-DML.sql
+++ eservice_jd/sql/INITIAL_ROLE-DML.sql
@@ -1,8 +1,6 @@
 INSERT INTO ESERVICE_ADM.DBO.ROLE
 (ROLE_ID,ROLE_NAME,DESCRIPTION,PARENT_ROLE_ID,DEPARTMENT_ID,CREATE_USER,CREATE_DATE,MODIFY_USER,MODIFY_DATE,DIV_ROLE_ID)
-VALUES
-    (
-        NEWID(),
+SELECT  NEWID(),
         '通路主管',
         '通路主管',
         '',
@@ -11,50 +9,23 @@ VALUES
         getdate(),
         'admin',
         getdate(),
-        'CHANNEL_LEADER'
-    )
-    insert INTO ESERVICE_ADM.DBO.ROLE_SYS_AUTH
-(ROLE_ID,
- SYS_ID,
- CREATE_USER,
- CREATE_DATE)
-VALUES ((select ROLE_ID from ESERVICE_ADM.dbo.ROLE WHERE DIV_ROLE_ID = 'CHANNEL_LEADER'),
-    'eservice_jd',
-    'admin',
-    getdate());
+        'CHANNEL_LEADER' FROM ESERVICE_JD.DBO.DEPARTMENT WHERE PARENT_DEP = 0
+union
+SELECT
+    NEWID(),
+            '分行主管',
+            '分行主管',
+            '',
+            '',
+            'admin',
+            getdate(),
+            'admin',
+            getdate(),
+            'BRANCH_LEADER' FROM ESERVICE_JD.DBO.DEPARTMENT WHERE PARENT_DEP = 0
 
-INSERT INTO ESERVICE_ADM.DBO.ROLE
-(ROLE_ID,ROLE_NAME,DESCRIPTION,PARENT_ROLE_ID,DEPARTMENT_ID,CREATE_USER,CREATE_DATE,MODIFY_USER,MODIFY_DATE,DIV_ROLE_ID)
-VALUES
-    (
-        NEWID(),
-        '分行主管',
-        '分行主管',
-        '',
-        '',
-        'admin',
-        getdate(),
-        'admin',
-        getdate(),
-        'BRANCH_LEADER'
-    )
-    insert INTO ESERVICE_ADM.DBO.ROLE_SYS_AUTH
-(ROLE_ID,
- SYS_ID,
- CREATE_USER,
- CREATE_DATE)
-VALUES ((select ROLE_ID from ESERVICE_ADM.dbo.ROLE WHERE DIV_ROLE_ID = 'BRANCH_LEADER'),
-    'eservice_jd',
-    'admin',
-    getdate());
-
-
-
-INSERT INTO ESERVICE_ADM.DBO.ROLE
-(ROLE_ID,ROLE_NAME,DESCRIPTION,PARENT_ROLE_ID,DEPARTMENT_ID,CREATE_USER,CREATE_DATE,MODIFY_USER,MODIFY_DATE,DIV_ROLE_ID)
-VALUES
-    (
-        NEWID(),
+union
+SELECT
+ NEWID(),
         '一般人員',
         '一般人員',
         '',
@@ -63,69 +34,71 @@ VALUES
         getdate(),
         'admin',
         getdate(),
-        'GENERAL_PERSONNEL'
-    )
-    insert INTO ESERVICE_ADM.DBO.ROLE_SYS_AUTH
+        'GENERAL_PERSONNEL' FROM ESERVICE_JD.DBO.DEPARTMENT WHERE PARENT_DEP = 0
+
+union
+SELECT
+  NEWID(),
+         'IC人員',
+         'IC人員',
+         '',
+         '',
+         'admin',
+         getdate(),
+         'admin',
+         getdate(),
+         'IC_PERSONNEL' FROM ESERVICE_JD.DBO.DEPARTMENT WHERE PARENT_DEP = 0
+
+union
+SELECT
+  NEWID(),
+         '平台管理人員',
+         '平台管理人員',
+         '',
+         '',
+         'admin',
+         getdate(),
+         'admin',
+         getdate(),
+         'ADMINISTRATOR' FROM ESERVICE_JD.DBO.DEPARTMENT WHERE PARENT_DEP = 0;
+
+
+INSERT INTO ESERVICE_ADM.DBO.ROLE_SYS_AUTH
 (ROLE_ID,
  SYS_ID,
  CREATE_USER,
  CREATE_DATE)
-VALUES ((select ROLE_ID from ESERVICE_ADM.dbo.ROLE WHERE DIV_ROLE_ID = 'GENERAL_PERSONNEL'),
-    'eservice_jd',
-    'admin',
-    getdate());
+SELECT ROLE_ID,'eservice_jd', 'admin', getdate()  from ESERVICE_ADM.dbo.ROLE WHERE DIV_ROLE_ID IN ('CHANNEL_LEADER', 'BRANCH_LEADER', 'GENERAL_PERSONNEL', 'IC_PERSONNEL', 'ADMINISTRATOR');
 
+insert ESERVICE_JD.dbo.ROLE_DEPARTMENT(ROLE_ID,a.DEP_ID)
+SELECT B.ROLE_ID, A.DEP_ID FROM (
+	select row_number() over(order by DEP_ID) as rownum, * from ESERVICE_JD.DBO.DEPARTMENT WHERE PARENT_DEP = 0
+) A LEFT JOIN (SELECT row_number() over(order by ROLE_ID) as rownum, * from ESERVICE_ADM.DBO.ROLE WHERE  DIV_ROLE_ID = 'CHANNEL_LEADER') B
+ON A.rownum = B.rownum;
 
+insert ESERVICE_JD.dbo.ROLE_DEPARTMENT(ROLE_ID,a.DEP_ID)
+SELECT B.ROLE_ID, A.DEP_ID FROM (
+	select row_number() over(order by DEP_ID) as rownum, * from ESERVICE_JD.DBO.DEPARTMENT WHERE PARENT_DEP = 0
+) A LEFT JOIN (SELECT row_number() over(order by ROLE_ID) as rownum, * from ESERVICE_ADM.DBO.ROLE WHERE  DIV_ROLE_ID = 'BRANCH_LEADER') B
+ON A.rownum = B.rownum;
 
-INSERT INTO ESERVICE_ADM.DBO.ROLE
-(ROLE_ID,ROLE_NAME,DESCRIPTION,PARENT_ROLE_ID,DEPARTMENT_ID,CREATE_USER,CREATE_DATE,MODIFY_USER,MODIFY_DATE,DIV_ROLE_ID)
-VALUES
-    (
-        NEWID(),
-        'IC人員',
-        'IC人員',
-        '',
-        '',
-        'admin',
-        getdate(),
-        'admin',
-        getdate(),
-        'IC_PERSONNEL'
-    )
-    insert INTO ESERVICE_ADM.DBO.ROLE_SYS_AUTH
-(ROLE_ID,
- SYS_ID,
- CREATE_USER,
- CREATE_DATE)
-VALUES ((select ROLE_ID from ESERVICE_ADM.dbo.ROLE WHERE DIV_ROLE_ID = 'IC_PERSONNEL'),
-    'eservice_jd',
-    'admin',
-    getdate());
+insert ESERVICE_JD.dbo.ROLE_DEPARTMENT(ROLE_ID,a.DEP_ID)
+SELECT B.ROLE_ID, A.DEP_ID FROM (
+	select row_number() over(order by DEP_ID) as rownum, * from ESERVICE_JD.DBO.DEPARTMENT WHERE PARENT_DEP = 0
+) A LEFT JOIN (SELECT row_number() over(order by ROLE_ID) as rownum, * from ESERVICE_ADM.DBO.ROLE WHERE  DIV_ROLE_ID = 'GENERAL_PERSONNEL') B
+ON A.rownum = B.rownum;
 
+insert ESERVICE_JD.dbo.ROLE_DEPARTMENT(ROLE_ID,a.DEP_ID)
+SELECT B.ROLE_ID, A.DEP_ID FROM (
+	select row_number() over(order by DEP_ID) as rownum, * from ESERVICE_JD.DBO.DEPARTMENT WHERE PARENT_DEP = 0
+) A LEFT JOIN (SELECT row_number() over(order by ROLE_ID) as rownum, * from ESERVICE_ADM.DBO.ROLE WHERE  DIV_ROLE_ID = 'IC_PERSONNEL') B
+ON A.rownum = B.rownum;
 
-INSERT INTO ESERVICE_ADM.DBO.ROLE
-(ROLE_ID,ROLE_NAME,DESCRIPTION,PARENT_ROLE_ID,DEPARTMENT_ID,CREATE_USER,CREATE_DATE,MODIFY_USER,MODIFY_DATE,DIV_ROLE_ID)
-VALUES
-    (
-        NEWID(),
-        '平台管理人員',
-        '平台管理人員',
-        '',
-        '',
-        'admin',
-        getdate(),
-        'admin',
-        getdate(),
-        'ADMINISTRATOR'
-    )
-    insert INTO ESERVICE_ADM.DBO.ROLE_SYS_AUTH
-(ROLE_ID,
- SYS_ID,
- CREATE_USER,
- CREATE_DATE)
-VALUES ((select ROLE_ID from ESERVICE_ADM.dbo.ROLE WHERE DIV_ROLE_ID = 'ADMINISTRATOR'),
-    'eservice_jd',
-    'admin',
-    getdate());
+insert ESERVICE_JD.dbo.ROLE_DEPARTMENT(ROLE_ID,a.DEP_ID)
+SELECT B.ROLE_ID, A.DEP_ID FROM (
+	select row_number() over(order by DEP_ID) as rownum, * from ESERVICE_JD.DBO.DEPARTMENT WHERE PARENT_DEP = 0
+) A LEFT JOIN (SELECT row_number() over(order by ROLE_ID) as rownum, * from ESERVICE_ADM.DBO.ROLE WHERE  DIV_ROLE_ID = 'ADMINISTRATOR') B
+ON A.rownum = B.rownum;
+
 
 
