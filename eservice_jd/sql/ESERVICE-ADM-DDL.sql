a39214abd0874240cf2cfe209abdc5bbbe948181
mrtang <re110119120@163.com>
2023-07-06 14:09:11 +0800

    update seq start

--- eservice_jd/sql/ESERVICE-ADM-DDL.sql
+++ eservice_jd/sql/ESERVICE-ADM-DDL.sql
@@ -4,6 +4,6 @@ alter table ESERVICE_ADM.dbo.ROLE add DIV_ROLE_ID  [nvarchar](50) NULL
 create sequence BUSINESS_EVENT_SEQ
 minvalue 1
 maxvalue 9223372036854775807
-start with 7000
+start with 88803
 increment by 1
 no cache;
